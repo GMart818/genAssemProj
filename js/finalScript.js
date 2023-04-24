7130c05c259af0bbd2105753742375f40d4580a9
Gabriel Martin <big_haas@gabriels-mbp.home>
2023-04-23 21:35:43 -0700

    Completed Final Project

--- js/finalScript.js
+++ js/finalScript.js
@@ -1,4 +1,3 @@
-
 const lengthInfoBtn = document.querySelector('#length-button');
 const shapeInfoBtn = document.querySelector('#shape-button');
 const finsInfoBtn = document.querySelector('#fins-button');
@@ -24,6 +23,10 @@ function displayPropperModal(event) {
 	return infoModal;
 }
 
+
+
+
+
 const gotItBtnLength = document.querySelector('#length-got-it');
 const gotItBtnShape = document.querySelector('#shape-got-it');
 const gotItBtnFins = document.querySelector('#fins-got-it');
@@ -36,3 +39,73 @@ function closeInfoModal() {
 	infoModal.classList.add('modal-off');	
 }
 
+let shapesModal;
+
+const shapesTableBtn = document.querySelector('#shapes-table');
+
+shapesTableBtn.addEventListener('click', openShapesModal);
+
+function openShapesModal(event) {
+	event.preventDefault();
+	if(event.target.innerText==="SHAPES TABLE") {
+		shapesModal = document.getElementById('shapes-modal');
+		shapesModal.classList.remove('modal-off');
+	}
+}
+
+const shapesTableGotItBtn = document.querySelector('#shapes-got-it');
+
+shapesTableGotItBtn.addEventListener('click', closeShapesModal);
+
+function closeShapesModal() {
+	shapesModal.classList.add('modal-off');	
+}
+
+
+
+
+
+
+const designForm = document.querySelector('#design-form');
+const errorsList = document.querySelector("#errors");
+
+designForm.addEventListener('submit', validateData);
+
+function validateData(event) {
+	errorsList.innerHTML = "";
+	event.preventDefault();
+	let designLength = document.querySelector('#length-in-feet').value;
+	
+	if (designLength === 'blank') {
+	    addError("Length cannot be blank.");
+  		}
+
+  	let designShape = document.querySelector('#shape-field').value;
+  	
+  	if (designShape === 'blank') {
+  		addError('Shape cannot be blank.');
+  	}
+
+  	let radios = document.getElementsByName("fins-input");
+  	let formValid = false;
+
+  	let i = 0;
+    while (!formValid && i < radios.length) {
+        if (radios[i].checked) formValid = true;
+        i++;        
+    }
+
+	if(!formValid) {
+		addError('You must select fin type.');
+	}			
+	else{
+	alert("You have successfully completed a design!");
+	designForm.reset();
+	}
+}
+
+function addError(error) {
+  let li = document.createElement("li");
+  li.innerText = error;
+  errorsList.appendChild(li);
+}
